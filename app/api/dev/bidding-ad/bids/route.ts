ad800d9049c951b394837d0295bad065de71992f
edlakin04 <edlakin2004@gmail.com>
2026-03-06 21:54:34 +0000

    Update route.ts

--- app/api/dev/bidding-ad/bids/route.ts
+++ app/api/dev/bidding-ad/bids/route.ts
@@ -39,10 +39,10 @@ function biddingAdScheduleForTargetDate(targetDate: string) {
     Date.UTC(prevDay.getUTCFullYear(), prevDay.getUTCMonth(), prevDay.getUTCDate(), 23, 0, 0, 0)
   );
   const auctionStartsAt = new Date(
-    Date.UTC(day.getUTCFullYear(), day.getUTCMonth(), day.getUTCDate(), 21, 20, 0, 0)
+    Date.UTC(day.getUTCFullYear(), day.getUTCMonth(), day.getUTCDate(), 22, 00, 0, 0)
   );
   const auctionEndsAt = new Date(
-    Date.UTC(day.getUTCFullYear(), day.getUTCMonth(), day.getUTCDate(), 22, 20, 0, 0)
+    Date.UTC(day.getUTCFullYear(), day.getUTCMonth(), day.getUTCDate(), 23, 00, 0, 0)
   );
 
   return {
